c46c50bafeda237e78d8c0e2c278943253e51f0e
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-07 19:11:11 -0500

    Add iterative and recursive Fibonacci implementations, and merge sort algorithm
    - Implemented an iterative approach for generating Fibonacci sequences (`fibs` function).
    - Implemented a recursive approach for generating Fibonacci sequences (`fibsRec` function).
    - Added a `mergeSort` function for sorting arrays using the merge sort algorithm.
    - Created a `merge` helper function to merge two sorted arrays in the merge sort implementation.
    - Added sample test cases using `console.table` to display sorted arrays.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-import './style.css';
+// Iterative approach
+function fibs(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+
+  const array = [0, 1];
+
+  for (let i = 2; i < num; i++) {
+    array.push(array[i - 1] + array[i - 2]);
+  }
+  return array;
+}
+
+// Recursive approach
+function fibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const array = fibsRec(n - 1);
+
+  array.push(array[array.length - 1] + array[array.length - 2]);
+
+  return array;
+}
+
+// MergeSort function
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  // Split array into two halve
+  const mid = Math.floor(array.length / 2);
+  const left = array.slice(0, mid);
+  const right = array.slice(mid);
+
+  // Recursively sort halve and merge them
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+// Function to merge arrays
+function merge(left, right) {
+  const mergedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  // Compare elements in sorted halves and merge
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      mergedArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // Add remaining elements from left and right halfs
+  for (; leftIndex < left.length; leftIndex++) {
+    mergedArray.push(left[leftIndex]);
+  }
+  for (; rightIndex < right.length; rightIndex++) {
+    mergedArray.push(right[rightIndex]);
+  }
+
+  return mergedArray;
+}
+
+console.table(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.table(mergeSort([105, 79, 100, 110, 6]));
